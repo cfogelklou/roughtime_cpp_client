ff7367faa8bf3658c00a42d52ca6c26c3b5c3c5f
Chris Fogelklou <chris.fogelklou@gmail.com>
2019-02-22 19:24:53 +0100

    Simple CURL setup and shutdown test started.

--- test/roughtime_test.cpp
+++ test/roughtime_test.cpp
@@ -1,3 +1,22 @@
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using namespace testing;
+
+TEST(TestCurl, curl_1) {
+  CURL * curl = curl_easy_init();
+  EXPECT_NE(nullptr, curl);
+  curl_easy_cleanup(curl);
+  
+}
+
 int main(int argc, char** argv){
-  return 0;
+  
+  // The following line must be executed to initialize Google Mock
+  // (and Google Test) before running the tests.
+  ::testing::InitGoogleMock(&argc, argv);
+  const int gtest_rval = RUN_ALL_TESTS();
+  
+  return gtest_rval;
 }
