50b058d01d2865b1238a9825cf32da33b07769d5
Chris Fogelklou <chris.fogelklou@polestar.com>
2019-03-07 12:16:25 +0100

    wip

--- test_w_node/app/main.js
+++ test_w_node/app/main.js
@@ -1,25 +1,6 @@
 // @flow
+import * as Roughtime from 'roughtime'
 
-import * as FirebaseApi from './firebase-api.js';
-import * as ossl from './openssl_utils';
-import * as debug from './debug';
-import {prompt} from './command_handler';
-
-import * as Logger from './logger';
-let logger = Logger.getInst();
-
-function VerifyOpenssl(){
-  let version = ossl.version();
-  let verIdx = version.indexOf('1.1.');
-  if ((verIdx >= 0) && (verIdx < version.length)){
-
-  }
-  else {
-    console.log("This tool requires openssl 1.1.x");
-    debug.assert(verIdx >= 0);
-    debug.assert(verIdx < version.length);
-  }
-}
 
 function trimQuotes(input:string):string{
   let out = input;
@@ -36,37 +17,10 @@ function trimQuotes(input:string):string{
 }
 
 export function go() {
-  // $FlowFixMe
-  console.log = logger.log;
-// $FlowFixMe
-  console.error = logger.error;
-
-  if (process.argv.length >= 4){
-
-    VerifyOpenssl();
-    let firebaseApi = new FirebaseApi.FirebaseApi();
-    let usr = trimQuotes(process.argv[2]);
-    let pass = trimQuotes(process.argv[3]);
-
-    firebaseApi.start(usr,pass, (started:boolean)=>{
-      if (started){
-        FirebaseApi.getLastProvisionedSN(
-          (sn:number)=>{
-            ossl.init(sn+1);
-          }
-        );
-      }
-      else {
-        console.log("Failed to log into firebase");
-      }
-    });
-
-    prompt(process.argv[4]);
-  }
-  else {
-    console.log("Enter username and password when starting process.")
-    console.log('fact_init <username> "<password>" "<port>"');
-    console.log('Ex: fact_init altran@altran.com password "/dev/tty.usbserial-FT9ZOUGE"');
-    console.log('<port> is optional.');
-  }
+  Roughtime('roughtime.cloudflare.com', (err, result) => {
+    if (err) throw err
+    const {midpoint, radius} = result
+    console.log(midpoint, radius) // ex. "1537907399109000 1000000"
+    console.log(new Date(midpoint / 1e3)) // ex. "2018-09-25T20:29:59.109Z"
+  })
 }
