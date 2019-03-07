de7f902a6893c3a1d53d520d34176d2652ad0918
Chris Fogelklou <chris.fogelklou@polestar.com>
2019-03-07 12:11:56 +0100

    wip

---
+++ test_w_node/debug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#You need to have run build_for_node.sh to "compile" to raw js, first.
+echo To debug this, open chrome://inspect
+node --inspect compiled/app/main_compiled.js
+#node --inspect-brk compiled/app/main_compiled.js
+
