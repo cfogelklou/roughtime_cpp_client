3423ba8779f2d89cc9d9259a8ff340dc06bce727
Chris Fogelklou <chris.fogelklou@polestar.com>
2019-03-07 14:25:07 +0100

    works.

--- test/mini_socket.cpp
+++ test/mini_socket.cpp
@@ -36,7 +36,7 @@ typedef int SOCKET;
 #define OSALSleep(x) usleep((uint64_t)(x)*1000ull)
 #endif
 
-static const int DEFAULT_BUFLEN = 32768;
+static const int DEFAULT_BUFLEN = 2048;
 #define MIN(x,y) (((x) < (y)) ? (x) : (y))
 
 // ////////////////////////////////////////////////////////////////////////////
@@ -101,17 +101,14 @@ public:
   : QueueBase()
   , mSocket(INVALID_SOCKET)
   , mRxByteQ()
-  , mRunning(true)
   , mPort(port)
-  , recvbuflen(sizeof(recvbuf))
-  , recvbuf{ 0 }
+  , mRxBuf{ 0 }
   {
     SerSocket::inst().Init();
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family     = AF_INET; // AF_INIT == internetwork: UDP, TCP, etc.
     hints.ai_socktype   = SOCK_DGRAM; /* datagram socket */
-    //hints.ai_flags    = AI_PASSIVE; // AI_PASSIVE == Socket address will be used in bind() call
     hints.ai_protocol   = IPPROTO_UDP;
 
     hints.ai_flags |= AI_ALL; // Query both IP6 and IP4 with AI_V4MAPPED
@@ -137,50 +134,15 @@ public:
       exit(-1);
     }
     printf("Socket created.\n");
-
-    if (0 != (hints.ai_flags & AI_PASSIVE)) {
-#if 1
-      //Prepare the sockaddr_in structure
-      static struct sockaddr_in server;
-      server.sin_family = AF_INET; // AF_INIT == internetwork: UDP, TCP, etc.
-      server.sin_addr.s_addr = INADDR_ANY;
-      server.sin_port = htons(mPort);
-
-      //Bind
-      if (bind(mSocket, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
-#else
-      if (bind(mSocket, rxResult->ai_addr, rxResult->ai_addrlen) == SOCKET_ERROR)
-#endif
-      {
-        //printf("Bind failed with error code : %d" , WSAGetLastError());
-        fprintf(stderr, "bind: %s\n", gai_strerror(mSocket));
-        exit(EXIT_FAILURE);
-      }
-      puts("Bind done");
-    }
-
-
-    //std::thread t(readFromSocketThreadC, this); // pass by reference
-    //t.detach();
-    
-    //if( connect(mSocket, rxResult->ai_addr, rxResult->ai_addrlen) == SOCKET_ERROR){
-    //  fprintf(stderr, "connect: %s\n", gai_strerror(mSocket));
-    //  exit(EXIT_FAILURE);
-    //}
-    
   }
 
+  // //////////////////////////////////////////////////////////////////////////
   ~SerSocketQueue(){
-    freeaddrinfo(rxResult);
-    mRunning = false;
+    freeaddrinfo(rxResult);    
   }
 
+  // //////////////////////////////////////////////////////////////////////////
   size_t Write(const uint8_t arr[], const size_t len) override {
-    CSTaskLocker cs;
-
-    /* Set family and port */
-    //server.sin_family = AF_INET;
-    //server.sin_port = htons(port_number);
 
     if (sendto(mSocket, (const char *)arr, len, 0, rxResult->ai_addr, rxResult->ai_addrlen) == SOCKET_ERROR) {
       auto err = WSAGetLastError();
@@ -192,76 +154,51 @@ public:
       static int timeout = 100;
       if (0 == setsockopt(mSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout))){
         int server_length = rxResult->ai_addrlen;
-        auto rx = recvfrom(mSocket, (char *)recvbuf, sizeof(recvbuf), 0, rxResult->ai_addr, &server_length);
+        auto rx = recvfrom(mSocket, (char *)mRxBuf, sizeof(mRxBuf), 0, rxResult->ai_addr, &server_length);
         if (SOCKET_ERROR != rx) {
-
+          mRxByteQ.Write(mRxBuf, rx);
         }
         else {
           auto err = WSAGetLastError();
           fprintf(stderr, "recv: %s\n", gai_strerror(err));
-          //printf("sendto() failed with error code : %d" , WSAGetLastError());
           exit(EXIT_FAILURE);
         }
       }
     }
     return 0;
   }
+
+  // //////////////////////////////////////////////////////////////////////////
   size_t Read(uint8_t arr[], const size_t len) override {
     return mRxByteQ.Read(arr, len);
   }
+
+  // //////////////////////////////////////////////////////////////////////////
   size_t GetWriteReady() override {
     return 2048;
   }
+  
+  // //////////////////////////////////////////////////////////////////////////
   size_t GetReadReady() override {
     return mRxByteQ.GetReadReady();
   }
 
-private:
-  // /////////////////////////////////
-  static void readFromSocketThreadC(void *pThis) {
-    ((SerSocketQueue *)pThis)->readFromSocketThread();
-  }
-
-  // /////////////////////////////////
-  void readFromSocketThread()
-  {
-    SOCKET clientSocket = mSocket;
-    // Receive until the peer shuts down the connection
-    int bytesReceived = 1;
-    while ((mRunning) && (clientSocket != INVALID_SOCKET) && (bytesReceived > 0)) {
-      bytesReceived = recv(clientSocket, (char *)recvbuf, recvbuflen - 1, 0);
-      if (bytesReceived > 0) {
-        CSTaskLocker cs;
-        auto amtWritten = mRxByteQ.Write(recvbuf, bytesReceived);
-        LOG_ASSERT_WARN(amtWritten == bytesReceived);
-      }
-      else if (bytesReceived == 0) {
-        // Do nothing
-      }
-      else {
-        LOG_TRACE(("recv failed with error\r\n"));
-      }
-    }
-    
-  }
 
 private:
   struct addrinfo *rxResult;
   SOCKET mSocket; ///< Wait for connections on this socket.
   ByteQ   mRxByteQ;
-  volatile bool mRunning;
   int mPort;
-  const size_t recvbuflen;
-  uint8_t recvbuf[DEFAULT_BUFLEN];
+  uint8_t mRxBuf[DEFAULT_BUFLEN];
 };
 
-
-
+// ////////////////////////////////////////////////////////////////////////////
 QueueBase &GetQueueToIp(const char *szAddr, const int port){
   auto p = new SerSocketQueue(szAddr, port);
   return *p;
 }
 
+// ////////////////////////////////////////////////////////////////////////////
 void ReleaseQueueToIp(QueueBase *p){
   SerSocketQueue *ps = (SerSocketQueue *)p;
   delete ps;
