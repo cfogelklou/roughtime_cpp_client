d8bb9a0c65615aa0f4682d05c949af5a7ad09b4b
Chris Fogelklou <chris.fogelklou@polestar.com>
2019-03-07 12:21:38 +0100

    Running works.

--- test_w_node/app/main.js
+++ test_w_node/app/main.js
@@ -1,23 +1,8 @@
 // @flow
-import * as Roughtime from 'roughtime'
-
-
-function trimQuotes(input:string):string{
-  let out = input;
-  let quote = '"'.charCodeAt(0);
-  let b = out.charCodeAt(0);
-  if (b === quote){
-    out = out.substr(1);
-  }
-  let l = out.charCodeAt(out.length-1)
-  if (l === quote){
-    out = out.substr(0, out.length-1);
-  }
-  return out;
-}
+const roughtime = require('roughtime')
 
 export function go() {
-  Roughtime('roughtime.cloudflare.com', (err, result) => {
+  roughtime('roughtime.cloudflare.com', (err, result) => {
     if (err) throw err
     const {midpoint, radius} = result
     console.log(midpoint, radius) // ex. "1537907399109000 1000000"
